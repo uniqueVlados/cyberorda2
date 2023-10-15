736afbf4b75c372a8c23d141cd2a02d10d809d83
uniqueVlados <vladbatsaev@mail.ru>
2023-10-15 16:32:49 +0300

    fix 1

--- blog/views.py
+++ blog/views.py
@@ -2210,19 +2210,19 @@ def schedule(request, pk):
                 com6_6.append([com_d[0][-1], "Пустышка"])
 
             if len(com_d[1]) % 2 != 0:
-                com6_5.append([com_d[0][-1], "Пустышка"])
+                com6_5.append([com_d[1][-1], "Пустышка"])
 
             if len(com_d[2]) % 2 != 0:
-                com6_4.append([com_d[0][-1], "Пустышка"])
+                com6_4.append([com_d[2][-1], "Пустышка"])
 
             if len(com_d[3]) % 2 != 0:
-                com6_3.append([com_d[0][-1], "Пустышка"])
+                com6_3.append([com_d[3][-1], "Пустышка"])
 
             if len(com_d[4]) % 2 != 0:
-                com6_2.append([com_d[0][-1], "Пустышка"])
+                com6_2.append([com_d[4][-1], "Пустышка"])
 
             if len(com_d[5]) % 2 != 0:
-                com6_1.append([com_d[0][-1], "Пустышка"])
+                com6_1.append([com_d[5][-1], "Пустышка"])
 
             file = open(f"{posts[0].title}/{posts[0].title}_тур6.txt", "r", encoding="utf-8")
             if len(file.read()) == 0:
